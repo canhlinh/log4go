0f0cfbab531ae91da2d9b0731b844dad8b427598
canhlinh <canhlinh.lienson@gmail.com>
2016-05-23 16:54:58 +0700

    Print color based on the log level.

---
+++ examples/ConsoleLogWriter_Manual.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"time"
+)
+
+import log "github.com/Innovatube/log4go"
+
+func main() {
+	defer log.Close()
+	log.AddFilter("stdout", log.DEBUG, log.NewConsoleLogWriter())
+	log.Info("The time is now: %s", time.Now().Format("15:04:05 MST 2006/01/02"))
+	log.Debug("The time is now: %s", time.Now().Format("15:04:05 MST 2006/01/02"))
+	log.Warn("The time is now: %s", time.Now().Format("15:04:05 MST 2006/01/02"))
+	log.Error("The time is now: %s", time.Now().Format("15:04:05 MST 2006/01/02"))
+	log.Trace("The time is now: %s", time.Now().Format("15:04:05 MST 2006/01/02"))
+}
---
+++ examples/FileLogWriter_Manual.go
@@ -0,0 +1,29 @@
+package main
+
+import "time"
+
+import l4g "github.com/Innovatube/log4go"
+
+const (
+	filename = "flw.log"
+)
+
+func main() {
+	// Get a new logger instance
+	log.Close()
+	// Create a default logger that is logging messages of FINE or higher
+
+	/* Can also specify manually via the following: (these are the defaults) */
+	flw := log.NewFileLogWriter(filename, false)
+	flw.SetFormat("[%D %T] [%L] (%S) %M")
+	flw.SetRotate(false)
+	flw.SetRotateSize(0)
+	flw.SetRotateLines(0)
+	flw.SetRotateDaily(false)
+	log.AddFilter("file", log.DEBUG, flw)
+	// Log some experimental messages
+	log.Finest("Everything is created now (notice that I will not be printing to the file)")
+	log.Info("The time is now: %s", time.Now().Format("15:04:05 MST 2006/01/02"))
+	log.Critical("Time to close out!")
+	log.Close()
+}
