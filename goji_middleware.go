fa22466fbd39d2fe22ee40c021a56466ebbe8261
canhlinh <canhlinh.lienson@gmail.com>
2016-05-24 12:01:31 +0700

    Replace log middle to goji.io

--- goji_middleware.go
+++ goji_middleware.go
@@ -4,32 +4,27 @@ import (
 	"fmt"
 	"net/http"
 	"time"
-
-	"github.com/zenazn/goji/web"
-	"github.com/zenazn/goji/web/middleware"
 )
 
 type gojiLogger struct {
 	h http.Handler
-	c *web.C
 }
 
 func (gmLog gojiLogger) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 	start := time.Now()
-	reqID := middleware.GetReqID(*gmLog.c)
-	Debug("[%s] Started %s '%s' from %s", reqID, req.Method, req.RequestURI, req.RemoteAddr)
+	Debug("Started %s '%s' from %s", req.Method, req.RequestURI, req.RemoteAddr)
 	lresp := wrapWriter(resp)
 
 	gmLog.h.ServeHTTP(lresp, req)
 	lresp.maybeWriteHeader()
 
 	latency := float64(time.Since(start)) / float64(time.Millisecond)
-	Debug("[%s] Returning %d in %s", reqID, lresp.status(), fmt.Sprintf("%6.4f ms", latency))
+	Debug("Returning %d in %s", lresp.status(), fmt.Sprintf("%6.4f ms", latency))
 }
 
-func NewGojiLog() func(*web.C, http.Handler) http.Handler {
-	fn := func(c *web.C, h http.Handler) http.Handler {
-		return gojiLogger{h: h, c: c}
+func NewGojiLog() func(http.Handler) http.Handler {
+	fn := func(h http.Handler) http.Handler {
+		return gojiLogger{h: h}
 	}
 	return fn
 }
