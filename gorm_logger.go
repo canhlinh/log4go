4c81e883afb3b787e8462b4d64021f0fc3ef27ee
canhlinh <canhlinh.lienson@gmail.com>
2016-11-22 15:25:42 +0700

    Format source file gorm

--- gorm_logger.go
+++ gorm_logger.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"reflect"
 	"regexp"
+	"strings"
 	"time"
 	"unicode"
 )
@@ -18,7 +19,8 @@ func (this GormLogger) Print(values ...interface{}) {
 	if len(values) > 1 {
 		level := values[0]
 		source := fmt.Sprintf("%v", values[1])
-		writeHttpLoging(source)
+		lastIndex := strings.LastIndex(source, "/") + 1
+		writeHttpLoging(source[lastIndex:])
 
 		if level == "sql" {
 			// duration
